170d7eb46ec3fdbda0802ced277ff05014760b0d
zoexyz73 <94219193+zoexyz73@users.noreply.github.com>
2022-07-31 20:25:52 +0700

    Update changelog.js

--- command/umum/changelog.js
+++ command/umum/changelog.js
@@ -69,8 +69,8 @@ module.exports = {
 					caption: str,
 					footer: config.namebot + " • " + config.ownername,
 					templateButtons: [
-						{ urlButton: { displayText: "Shortlink", url: "https://sl.rzkyfdlh.tech" } },
-						{ urlButton: { displayText: "Downloader", url: "https://downloader.rzkyfdlh.tech" } },
+						/* { urlButton: { displayText: "Shortlink", url: "https://sl.rzkyfdlh.tech" } }, */
+						/* { urlButton: { displayText: "Downloader", url: "https://downloader.rzkyfdlh.tech" } }, */
 						{ quickReplyButton: { displayText: "Script Bot📑", id: "#script" } },
 						{ quickReplyButton: { displayText: "Owner👥", id: "#owner" } },
 						{ quickReplyButton: { displayText: "Dashboard📊", id: "#db" } },
